5e76e6606ae4a2b8559d05b8812dd8ac11c5eacf
Simone Spedicato <simonespedicatospf@gmail.com>
2025-06-17 17:27:35 +0200

    Structure Report, Implemented LCG

---
+++ prngs/lcg.py
@@ -0,0 +1,9 @@
+# Implementation of a Linear congruential generator from Wikipedia in form of a generator
+
+from collections.abc import Generator
+
+def lcg(modulus: int, a: int, c: int, seed: int) -> Generator[int, None, None]:
+    """Linear congruential generator."""
+    while True:
+        seed = (a * seed + c) % modulus
+        yield seed
